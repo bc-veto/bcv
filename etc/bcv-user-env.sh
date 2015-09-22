648203742644121b21e3b6f69288824c8dbbb229
Nairwita Mazumder <nairwita.mazumder@ligo.org>
2015-09-21 23:20:00 -0500

    cleaning repo

--- etc/bcv-user-env.sh
+++
@@ -1,8 +0,0 @@
-# Source this file to access BCV
-BCV_PREFIX=/home/bernard.hall/local/bilinear/omega_veto
-export BCV_PREFIX
-PATH=/home/bernard.hall/local/bilinear/omega_veto/bin:${PATH}
-PYTHONPATH=/home/bernard.hall/local/bilinear/omega_veto/lib64/python2.6/site-packages:/home/bernard.hall/local/bilinear/omega_veto/lib/python2.6/site-packages:${PYTHONPATH}
-LD_LIBRARY_PATH=/home/bernard.hall/local/bilinear/omega_veto/lib64/python2.6/site-packages:${LD_LIBRARY_PATH}
-DYLD_LIBRARY_PATH=/home/bernard.hall/local/bilinear/omega_veto/lib64/python2.6/site-packages:${DYLD_LIBRARY_PATH}
-export PATH PYTHONPATH LD_LIBRARY_PATH DYLD_LIBRARY_PATH
