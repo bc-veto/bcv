89120b0f33e5df0efd98045d98dd999fd420f6c4
Sudarshan <sudu.ghonge@gmail.com>
2015-11-11 19:23:03 +0530

    Minor bug fixes

--- srcPkg/bcv.py
+++ srcPkg/bcv.py
@@ -162,7 +162,7 @@ def linearCouplingCoeff(dataH, dataX, timeH, timeX, transFnXtoH, segStartTime,
 	sys.exit('Inconsistent dimensions of data and transfer function')
       
       
-      [rXH, rMaxXH] = calcrossCorr(xPrime, fftChan)
+      [rXH, rMaxXH] = calCrossCorr(xPrime, fftChan)
       return [rH, rMaxXH]
     
     
